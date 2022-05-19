a8963b3a3c3ab041e137a39f11ccfdbde4cfa710
Kiran Kumbhar <kiranpk189@gmail.com>
2022-05-19 11:35:28 +0530

    Added websocket message handler

--- websocketclient/handlers.py
+++ websocketclient/handlers.py
@@ -1,10 +1,28 @@
+from cgitb import handler
+import importlib
+
 from django.conf import settings
 
 
-async def message_handler(message):
-    print(message)
+async def default_message_handler(message):
+    pass
 
 
-message_handler_func = getattr(
-    settings, "WEBSOCKETCLIENT_MESSAGE_HANDLER", message_handler
-)
+def get_handler_from_settings():
+    _handler = getattr(settings, "WEBSOCKETCLIENT_MESSAGE_HANDLER")
+    if _handler is None:
+        return
+    if not isinstance(_handler, str):
+        return
+    handler_module, handler_func_name = _handler.rsplit(".", 1)
+    module = importlib.import_module(handler_module)
+    handler_func = getattr(module, handler_func_name)
+    if handler_func is None:
+        return
+    return handler_func
+
+
+message_handler_func = get_handler_from_settings()
+
+if not message_handler_func:
+    message_handler_func = default_message_handler
